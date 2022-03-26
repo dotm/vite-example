faa559c82d2f42ca6f457e2c6a7ffda3cf563333
dotm <desde.el.mar96@gmail.com>
2022-03-26 16:31:35 +0700

    add formik and yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import * as React from "react";
+import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -7,6 +9,7 @@ function App() {
       <h1>Welcome to React Router!</h1>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="signup" element={<SignupForm />} />
         <Route path="about" element={<About />} />
       </Routes>
     </div>
@@ -20,8 +23,9 @@ function Home() {
         <h2>Welcome to the homepage!</h2>
         <p>You can do this, I believe in you.</p>
       </main>
-      <nav>
-        <Link to="/about" className="link">About</Link>
+      <nav className='tabs justify-center'>
+        <Link to="/about" className="tab">About</Link>
+        <Link to="/signup" className="tab">Sign Up</Link>
       </nav>
     </>
   );
@@ -43,5 +47,60 @@ function About() {
     </>
   );
 }
+
+const SignupForm = () => {
+  return (
+    <Formik
+      initialValues={{ firstName: '', lastName: '', email: '' }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(15, 'Must be 15 characters or less')
+          .required('Required'),
+        lastName: Yup.string()
+          .max(20, 'Must be 20 characters or less')
+          .required('Required'),
+        email: Yup.string().email('Invalid email address').required('Required'),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {formik => (
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            id="firstName"
+            type="text"
+            {...formik.getFieldProps('firstName')}
+          />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <div>{formik.errors.firstName}</div>
+          ) : null}
+
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            id="lastName"
+            type="text"
+            {...formik.getFieldProps('lastName')}
+          />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <div>{formik.errors.lastName}</div>
+          ) : null}
+
+          <label htmlFor="email">Email Address</label>
+          <input id="email" type="email" {...formik.getFieldProps('email')} />
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
+
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </Formik>
+  );
+};
 
 export default App
